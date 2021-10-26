c1db885b50868da7ec81e864b7eb55c646061272
malinnilsson87 <69352382+malinnilsson87@users.noreply.github.com>
2021-10-26 21:47:05 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+
+  let button = document.querySelector("button");
+  let textdiv = document.getElementById("text-input");
+  const myArray = ["Asking for help is a sign of self-respect and self-awareness.", 
+  "I am in charge of how I feel and I choose to feel happy.", 
+  "I am optimistic because today is a new day.", 
+  "I breathe in trust, I exhale doubt.",
+  "I celebrate the good qualities in others and myself.",
+  "I do not rush through my life, I temper speed with stillness.",
+  "I do not pretend to be anyone or anything other than who I am.",
+  "I celebrate the good qualities in others and myself.",
+  "I strive for joy, not for perfection.",
+];
+
+  button.addEventListener("click", () => generateAffirmation(myArray));
+
+  function generateAffirmation(array) {
+    let randomNum = Math.floor(Math.random() * array.length);
+    textdiv.innerHTML = array[randomNum];
+  }
+}
