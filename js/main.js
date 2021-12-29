3ba5aa081604199e841d74d8c220f32b63cd76e5
Malin Nilsson <malinnilsson87@gmail.com>
2021-12-29 19:30:07 +0100

    Added mixins and more styling

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 const reminders = ["Asking for help is a sign of self-respect and self-awareness.",
   "I am in charge of how I feel and I choose to feel happy.",
   "I am optimistic because today is a new day.",
-  "I breathe in trust, I exhale doubt.",
-  "I celebrate the good qualities in others and myself.",
-  "I do not rush through my life, I temper speed with stillness.",
   "I do not pretend to be anyone or anything other than who I am.",
   "I celebrate the good qualities in others and myself.",
-  "  I breathe in calmness and breathe out nervousness.",
+  "I breathe in calmness and breathe out nervousness.",
   "I strive for joy, not for perfection.",
 ];
 
@@ -16,8 +13,10 @@ window.onload = function () {
 }
 
 function generateAffirmation(reminders) {
-  let reminderContainer = document.getElementById("reminder");
+  let reminderContainer = document.querySelector(".reminder-container");
   reminderContainer.style.display = "block";
+  let reminder = document.getElementById("reminder");
+  reminder.style.display = "block";
   let randomNum = Math.floor(Math.random() * reminders.length);
-  reminderContainer.innerHTML = reminders[randomNum];
+  reminder.innerHTML = reminders[randomNum];
 }
